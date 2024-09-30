7fd83a39d09fc27752e723aea3c90d14a8eda581
ansaryshohan <ansary2083@gmail.com>
2024-09-30 19:27:07 +0600

    project requirement done

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,16 @@ use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'master.pages.homePage');
-Route::get('/clients',[ClientController::class, "index"]);
-Route::get('/client/add',[ClientController::class, "create"]);
-Route::post('/client',[ClientController::class, "store"]);
-Route::get('/client/{id}',[ClientController::class, "show"]);
-Route::get('/client/{id}/edit',[ClientController::class, "edit"]);
-Route::patch('/client/{id}',[ClientController::class, "update"]);
-Route::delete('/client/{id}',[ClientController::class, "destroy"]);
+Route::middleware("auth")->group(function () {
+    Route::view('/', 'master.pages.homePage');
+    Route::get('/clients', [ClientController::class, "index"]);
+    Route::get('/client/add', [ClientController::class, "create"]);
+    Route::post('/client', [ClientController::class, "store"]);
+    Route::get('/client/{id}', [ClientController::class, "show"]);
+    Route::get('/client/{id}/edit', [ClientController::class, "edit"]);
+    Route::patch('/client/{id}', [ClientController::class, "update"]);
+    Route::delete('/client/{id}', [ClientController::class, "destroy"]);
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
